b6df8fd5dab703ca8d9da565854dd90452c9de13
dezfuture <agrim2000@gmail.com>
2021-11-26 23:25:05 +0530

    Co-authored-by: akk312000 <akk312000@gmail.com>

---
+++ src/components/Index/styles.js
@@ -0,0 +1,14 @@
+// const styles = theme => ({
+//   root: {
+//     backgroundColor: "red"
+//     },
+//     container: {
+//         padding: "100px",
+//     },
+//     appBar: {
+//         borderRadius: 20,
+
+//     }
+// });
+
+// export default styles;

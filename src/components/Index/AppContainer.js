7832ec7c106d340c01d99b2fe1f7c7b54ec9da13
jackie <99sharmatushar@gmail.com>
2021-11-28 00:56:47 +0530

    refactoring done

--- src/components/Index/AppContainer.js
+++ src/components/Index/AppContainer.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { main } from "../scripts/client";
 
-import classes from './styles.module.css';
-import { Icon } from 'semantic-ui-react'
+import classes from "./styles.module.css";
+import { Icon } from "semantic-ui-react";
 
 export default class AppContainer extends Component {
   // componentDidMount(){
@@ -15,114 +15,153 @@ export default class AppContainer extends Component {
     meetingCode: "",
   };
 
-    handleSubmit = () => {
-        main(this.state.name);
-        this.setState({showHome: false});
-    }
-    
-    render() {
-        
-        return (this.state.showHome?(
-        <div className={classes.parent}>
-            <h1 className={classes.heading}>Gozzip</h1>
-            <form onSubmit={this.handleSubmit}>
-                <div>
-                    <p className={classes.para}>Enter your name</p>
-                    <input className={classes.input} placeholder="UserName" required type="text" value={this.state.name} onChange={(event)=>{this.setState({name: event.target.value})}} />
-                    <p className={classes.para}>Create/Enter Meeting Code</p>
-                    <input className={classes.input} placeholder="Meeting Code" required type="text" value={this.state.meetingCode} onChange={(event)=>{this.setState({meetingCode: event.target.value})}} />
-                    <br/><br/>
-                </div>
-                <input className={classes.done} type="submit" value="Submit" />
-            </form>
+  handleSubmit = () => {
+    main(this.state.name);
+    this.setState({ showHome: false });
+  };
+
+  render() {
+    return this.state.showHome ? (
+      <div className={classes.parent}>
+        <h1 className={classes.heading}>Gozzip</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <p className={classes.para}>Enter your name</p>
+            <input
+              className={classes.input}
+              placeholder="UserName"
+              required
+              type="text"
+              value={this.state.name}
+              onChange={(event) => {
+                this.setState({ name: event.target.value });
+              }}
+            />
+            <p className={classes.para}>Create/Enter Meeting Code</p>
+            <input
+              className={classes.input}
+              placeholder="Meeting Code"
+              required
+              type="text"
+              value={this.state.meetingCode}
+              onChange={(event) => {
+                this.setState({ meetingCode: event.target.value });
+              }}
+            />
+            <br />
+            <br />
+          </div>
+          <input className={classes.done} type="submit" value="Submit" />
+        </form>
+      </div>
+    ) : (
+      <div>
+        <h1 className={classes.heading}>
+          <Icon name="conversation" />
+          Gozzip
+        </h1>
+
+        <div style={{ marginTop: "30px" }}>
+          <div
+            id="name-message"
+            style={{
+              float: "left",
+              width: "50%",
+              fontSize: "22px",
+              color: "#4671b5",
+            }}
+          >
+            Logging in...
+          </div>
+
+          <div
+            style={{
+              float: "left",
+              width: "50%",
+              fontSize: "22px",
+              color: "#4671b5",
+            }}
+          >
+            Meeting Id:<b>#{this.state.meetingCode}</b>
+          </div>
         </div>
-        ):(
-            <div>
-                Gozzip
-                <div id="app">
-        <h1 id="name-message">Logging in...</h1>
 
-          {/* <!-- Join or Leave a Conference --> */}
-          <div id="form">
-            <label>Conference alias :</label>
-            <input id="alias-input" value={this.state.meetingCode} />
-            <button id="join-btn" disabled>
-             <span class="material-icons">
-video_call
-</span>
-            </button>
-            <button id="leave-btn" disabled>
-             <span class="material-icons">
-call_end
-</span>
-            </button>
-            <label id="label-dolby-voice"></label>
+        <div id="app" style={{ width: "100%", marginTop: "120px" }}>
+          <div style={{ float: "left", width: "80%" }}>
+            {/* <!-- Display the list of participants --> */}
+            <div id="participants">
+              <h3>
+                <span class="material-icons">people_outline</span>Participants
+              </h3>
+              <ul id="participants-list"></ul>
+              {/* <!-- Container for the video streams --> */}
+              <div id="video-container"></div>
+
+              {/* <!-- Container for the Screen Share --> */}
+              <div id="screenshare-container"></div>
+            </div>
           </div>
 
-          {/* <!-- Actions on Video and Screen Share --> */}
-          <div id="actions">
-            <button id="start-video-btn" disabled>
-             <span class="material-icons">
-videocam
-</span>
-            </button>
-            <button id="stop-video-btn" disabled>
+          <div
+            style={{
+              float: "left",
+              width: "20%",
+              height: "100%",
+            }}
+          >
+            {/* <!-- Join or Leave a Conference --> */}
+            <div style={{ marginTop: "50%" }}>
+              <div id="form">
+                <input id="alias-input" value={this.state.meetingCode} hidden />
+                <button id="join-btn" disabled>
+                  <span class="material-icons">video_call</span>
+                </button>
+                <button id="leave-btn" disabled>
+                  <span class="material-icons">call_end</span>
+                </button>
+                <label id="label-dolby-voice"></label>
+              </div>
 
-<span class="material-icons">
-videocam_off
-</span>
+              {/* <!-- Actions on Video and Screen Share --> */}
+              <div id="actions">
+                <button id="start-video-btn" disabled>
+                  <span class="material-icons">videocam</span>
+                </button>
+                <button id="stop-video-btn" disabled>
+                  <span class="material-icons">videocam_off</span>
+                </button>
+                <br />
+                <button id="start-audio-btn" disabled>
+                  <span class="material-icons">mic</span>
+                </button>
 
-            </button>
-            <button id="start-audio-btn" disabled>
-             <span class="material-icons">
-mic
-</span>
-            </button>
+                <button id="stop-audio-btn" disabled>
+                  {" "}
+                  <span class="material-icons">mic_off</span>
+                </button>
+                <br />
+                <button id="start-screenshare-btn" disabled>
+                  <span class="material-icons">screen_share</span>
+                </button>
+                <button id="stop-screenshare-btn" disabled>
+                  <span class="material-icons">stop_screen_share</span>
+                </button>
+              </div>
 
-            <button id="stop-audio-btn" disabled>
-              {" "}
-              <span class="material-icons">
-mic_off
-</span>
-            </button>
-            <button id="start-screenshare-btn" disabled>
-              <span class="material-icons">screen_share</span>
-            </button>
-            <button id="stop-screenshare-btn" disabled>
-              <span class="material-icons">stop_screen_share</span>
-            </button>
+              {/* <!-- Recording Actions and Status --> */}
+              <div id="recording">
+                <button id="start-recording-btn" disabled>
+                  <span class="material-icons">radio_button_unchecked</span>
+                </button>
+                <button id="stop-recording-btn" disabled>
+                  <span class="material-icons">radio_button_checked</span>
+                </button>
+                <p id="record-status" style={{ color: "red" }}></p>
+              </div>
+            </div>
           </div>
-
-          {/* <!-- Recording Actions and Status --> */}
-          <div id="recording">
-            <button id="start-recording-btn" disabled>
-              <span class="material-icons">
-radio_button_unchecked
-</span>
-            </button>
-            <button id="stop-recording-btn" disabled>
-            <span class="material-icons">
-radio_button_checked
-</span>
-            </button>
-            <p id="record-status" style={{ color: "red" }}></p>
-          </div>
-
-          {/* <!-- Display the list of participants --> */}
-          <div id="participants">
-            <h3><span class="material-icons">
-people_outline
-</span>Participants</h3>
-            <ul id="participants-list"></ul>
-          </div>
-
-          {/* <!-- Container for the video streams --> */}
-          <div id="video-container"></div>
-
-          {/* <!-- Container for the Screen Share --> */}
-          <div id="screenshare-container"></div>
         </div>
       </div>
-    ));
+    );
   }
 }
--- src/components/scripts/ui.js
+++ src/components/scripts/ui.js
@@ -1,206 +1,213 @@
 import VoxeetSDK from "@voxeet/voxeet-web-sdk";
 
 export const initUI = (randomName) => {
-    const nameMessage = document.getElementById('name-message');
-    const joinButton = document.getElementById('join-btn');
-    const conferenceAliasInput = document.getElementById('alias-input');
-    const leaveButton = document.getElementById('leave-btn');
-    const lblDolbyVoice = document.getElementById('label-dolby-voice');
-    const startVideoBtn = document.getElementById('start-video-btn');
-    const stopVideoBtn = document.getElementById('stop-video-btn');
-    const startAudioBtn = document.getElementById('start-audio-btn');
-    const stopAudioBtn = document.getElementById('stop-audio-btn');
-    const startScreenShareBtn = document.getElementById('start-screenshare-btn');
-    const stopScreenShareBtn = document.getElementById('stop-screenshare-btn');
-    const startRecordingBtn = document.getElementById('start-recording-btn');
-    const stopRecordingBtn = document.getElementById('stop-recording-btn');
-  
-    // Update the login message with the name of the user
-    nameMessage.innerHTML = `You are logged in as ${randomName}`;
-    joinButton.disabled = false;
-  
-    joinButton.onclick = () => {
-      // Default conference parameters
-      // See: https://docs.dolby.io/interactivity/docs/js-client-sdk-model-conferenceparameters
-      let conferenceParams = {
-        liveRecording: false,
-        rtcpMode: "average", // worst, average, max
-        ttl: 0,
-        videoCodec: "H264", // H264, VP8
-        dolbyVoice: true
-      };
-  
-      // See: https://docs.dolby.io/interactivity/docs/js-client-sdk-model-conferenceoptions
-      let conferenceOptions = {
-        alias: conferenceAliasInput.value,
-        params: conferenceParams
-      };
-  
-      // 1. Create a conference room with an alias
-      VoxeetSDK.conference.create(conferenceOptions)
-        .then((conference) => {
-          // See: https://docs.dolby.io/interactivity/docs/js-client-sdk-model-joinoptions
-          const joinOptions = {
-            constraints: {
-              audio: true,
-              video: false
-            },
-            simulcast: false
-          };
-  
-          // 2. Join the conference
-          VoxeetSDK.conference.join(conference, joinOptions)
-            .then((conf) => {
-              lblDolbyVoice.innerHTML = `Dolby Voice is ${conf.params.dolbyVoice ? 'On' : 'Off'}.`;
-  
-              conferenceAliasInput.disabled = true;
-              joinButton.disabled = true;
-              leaveButton.disabled = false;
-              startVideoBtn.disabled = false;
-              startAudioBtn.disabled = true;
-              stopAudioBtn.disabled = false;
-              startScreenShareBtn.disabled = false;
-              startRecordingBtn.disabled = false;
-            })
-            .catch((err) => console.error(err));
-        })
-        .catch((err) => console.error(err));
+  const nameMessage = document.getElementById("name-message");
+  const joinButton = document.getElementById("join-btn");
+  const conferenceAliasInput = document.getElementById("alias-input");
+  const leaveButton = document.getElementById("leave-btn");
+  const lblDolbyVoice = document.getElementById("label-dolby-voice");
+  const startVideoBtn = document.getElementById("start-video-btn");
+  const stopVideoBtn = document.getElementById("stop-video-btn");
+  const startAudioBtn = document.getElementById("start-audio-btn");
+  const stopAudioBtn = document.getElementById("stop-audio-btn");
+  const startScreenShareBtn = document.getElementById("start-screenshare-btn");
+  const stopScreenShareBtn = document.getElementById("stop-screenshare-btn");
+  const startRecordingBtn = document.getElementById("start-recording-btn");
+  const stopRecordingBtn = document.getElementById("stop-recording-btn");
+
+  // Update the login message with the name of the user
+  nameMessage.innerHTML = `You are logged in as ${randomName}`;
+  joinButton.disabled = false;
+
+  joinButton.onclick = () => {
+    // Default conference parameters
+    // See: https://docs.dolby.io/interactivity/docs/js-client-sdk-model-conferenceparameters
+    let conferenceParams = {
+      liveRecording: true,
+      rtcpMode: "average", // worst, average, max
+      ttl: 0,
+      videoCodec: "H264", // H264, VP8
+      dolbyVoice: true,
     };
-  
-    leaveButton.onclick = () => {
-      // Leave the conference
-      VoxeetSDK.conference.leave()
-        .then(() => {
-          lblDolbyVoice.innerHTML = '';
-  
-          conferenceAliasInput.disabled = false;
-          joinButton.disabled = false;
-          leaveButton.disabled = true;
-          startVideoBtn.disabled = true;
-          stopVideoBtn.disabled = true;
-          startAudioBtn.disabled = true;
-          stopAudioBtn.disabled = true;
-          startScreenShareBtn.disabled = true;
-          stopScreenShareBtn.disabled = true;
-          startRecordingBtn.disabled = true;
-          stopRecordingBtn.disabled = true;
-        })
-        .catch((err) => console.error(err));
+
+    // See: https://docs.dolby.io/interactivity/docs/js-client-sdk-model-conferenceoptions
+    let conferenceOptions = {
+      alias: conferenceAliasInput.value,
+      params: conferenceParams,
     };
-  
-    startVideoBtn.onclick = () => {
-      // Start sharing the video with the other participants
-      VoxeetSDK.conference.startVideo(VoxeetSDK.session.participant)
-        .then(() => {
-          startVideoBtn.disabled = true;
-          stopVideoBtn.disabled = false;
-        })
-        .catch((err) => console.error(err));
-    };
-  
-    stopVideoBtn.onclick = () => {
-      // Stop sharing the video with the other participants
-      VoxeetSDK.conference.stopVideo(VoxeetSDK.session.participant)
-        .then(() => {
-          stopVideoBtn.disabled = true;
-          startVideoBtn.disabled = false;
-        })
-        .catch((err) => console.error(err));
-    };
-  
-    startAudioBtn.onclick = () => {
-      // Start sharing the Audio with the other participants
-      VoxeetSDK.conference.startAudio(VoxeetSDK.session.participant)
-        .then(() => {
-          startAudioBtn.disabled = true;
-          stopAudioBtn.disabled = false;
-        })
-        .catch((err) => console.error(err));
-    };
-  
-    stopAudioBtn.onclick = () => {
-      // Stop sharing the Audio with the other participants
-      VoxeetSDK.conference.stopAudio(VoxeetSDK.session.participant)
-        .then(() => {
-          stopAudioBtn.disabled = true;
-          startAudioBtn.disabled = false;
-        })
-        .catch((err) => console.error(err));
-    };
-  
-    startScreenShareBtn.onclick = () => {
-      // Start the Screen Sharing with the other participants
-      VoxeetSDK.conference.startScreenShare()
-        .then(() => {
-          startScreenShareBtn.disabled = true;
-          stopScreenShareBtn.disabled = false;
-        })
-        .catch((err) => console.error(err));
-    };
-  
-    stopScreenShareBtn.onclick = () => {
-      // Stop the Screen Sharing
-      VoxeetSDK.conference.stopScreenShare()
-        .catch((err) => console.error(err));
-    };
-  
-    startRecordingBtn.onclick = () => {
-      let recordStatus = document.getElementById('record-status');
-      
-      // Start recording the conference
-      VoxeetSDK.recording.start()
-        .then(() => {
-          recordStatus.innerText = 'Recording...';
-          startRecordingBtn.disabled = true;
-          stopRecordingBtn.disabled = false;
-        })
-        .catch((err) => console.error(err));
-    };
-  
-    stopRecordingBtn.onclick = () => {
-      let recordStatus = document.getElementById('record-status');
-  
-      // Stop recording the conference
-      VoxeetSDK.recording.stop()
-        .then(() => {
-          recordStatus.innerText = '';
-          startRecordingBtn.disabled = false;
-          stopRecordingBtn.disabled = true;
-        })
-        .catch((err) => console.error(err));
-    };
-  
+
+    // 1. Create a conference room with an alias
+    VoxeetSDK.conference
+      .create(conferenceOptions)
+      .then((conference) => {
+        // See: https://docs.dolby.io/interactivity/docs/js-client-sdk-model-joinoptions
+        const joinOptions = {
+          constraints: {
+            audio: true,
+            video: false,
+          },
+          simulcast: false,
+        };
+
+        // 2. Join the conference
+        VoxeetSDK.conference
+          .join(conference, joinOptions)
+          .then((conf) => {
+            // lblDolbyVoice.innerHTML = `Dolby Voice is ${
+            //   conf.params.dolbyVoice ? "On" : "Off"
+            // }.`;
+
+            conferenceAliasInput.disabled = true;
+            joinButton.disabled = true;
+            leaveButton.disabled = false;
+            startVideoBtn.disabled = false;
+            startAudioBtn.disabled = true;
+            stopAudioBtn.disabled = false;
+            startScreenShareBtn.disabled = false;
+            startRecordingBtn.disabled = false;
+          })
+          .catch((err) => console.error(err));
+      })
+      .catch((err) => console.error(err));
   };
-  
-  // Add a video stream to the web page
-  // Remove the video streem from the web page
-  // const removeVideoNode = (participant) => {
-  //   let videoNode = document.getElementById('video-' + participant.id);
-  
-  //   if (videoNode) {
-  //     videoNode.srcObject = null; // Prevent memory leak in Chrome
-  //     videoNode.parentNode.removeChild(videoNode);
-  //   }
-  // };
-  
-  // Add a new participant to the list
-  // const addParticipantNode = (participant) => {
-  //   // If the participant is the current session user, don't add them to the list
-  //   if (participant.id === VoxeetSDK.session.participant.id) return;
-  
-  //   let participantNode = document.createElement('li');
-  //   participantNode.setAttribute('id', 'participant-' + participant.id);
-  //   participantNode.innerText = `${participant.info.name}`;
-  
-  //   const participantsList = document.getElementById('participants-list');
-  //   participantsList.appendChild(participantNode);
-  // };
-  
-  // Remove a participant from the list
-  
-  // Add a screen share stream to the web page
 
+  leaveButton.onclick = () => {
+    // Leave the conference
+    VoxeetSDK.conference
+      .leave()
+      .then(() => {
+        lblDolbyVoice.innerHTML = "";
 
+        conferenceAliasInput.disabled = false;
+        joinButton.disabled = false;
+        leaveButton.disabled = true;
+        startVideoBtn.disabled = true;
+        stopVideoBtn.disabled = true;
+        startAudioBtn.disabled = true;
+        stopAudioBtn.disabled = true;
+        startScreenShareBtn.disabled = true;
+        stopScreenShareBtn.disabled = true;
+        startRecordingBtn.disabled = true;
+        stopRecordingBtn.disabled = true;
+      })
+      .catch((err) => console.error(err));
+  };
 
+  startVideoBtn.onclick = () => {
+    // Start sharing the video with the other participants
+    VoxeetSDK.conference
+      .startVideo(VoxeetSDK.session.participant)
+      .then(() => {
+        startVideoBtn.disabled = true;
+        stopVideoBtn.disabled = false;
+      })
+      .catch((err) => console.error(err));
+  };
 
-  // Remove the screen share stream from the web page
+  stopVideoBtn.onclick = () => {
+    // Stop sharing the video with the other participants
+    VoxeetSDK.conference
+      .stopVideo(VoxeetSDK.session.participant)
+      .then(() => {
+        stopVideoBtn.disabled = true;
+        startVideoBtn.disabled = false;
+      })
+      .catch((err) => console.error(err));
+  };
+
+  startAudioBtn.onclick = () => {
+    // Start sharing the Audio with the other participants
+    VoxeetSDK.conference
+      .startAudio(VoxeetSDK.session.participant)
+      .then(() => {
+        startAudioBtn.disabled = true;
+        stopAudioBtn.disabled = false;
+      })
+      .catch((err) => console.error(err));
+  };
+
+  stopAudioBtn.onclick = () => {
+    // Stop sharing the Audio with the other participants
+    VoxeetSDK.conference
+      .stopAudio(VoxeetSDK.session.participant)
+      .then(() => {
+        stopAudioBtn.disabled = true;
+        startAudioBtn.disabled = false;
+      })
+      .catch((err) => console.error(err));
+  };
+
+  startScreenShareBtn.onclick = () => {
+    // Start the Screen Sharing with the other participants
+    VoxeetSDK.conference
+      .startScreenShare()
+      .then(() => {
+        startScreenShareBtn.disabled = true;
+        stopScreenShareBtn.disabled = false;
+      })
+      .catch((err) => console.error(err));
+  };
+
+  stopScreenShareBtn.onclick = () => {
+    // Stop the Screen Sharing
+    VoxeetSDK.conference.stopScreenShare().catch((err) => console.error(err));
+  };
+
+  startRecordingBtn.onclick = () => {
+    let recordStatus = document.getElementById("record-status");
+
+    // Start recording the conference
+    VoxeetSDK.recording
+      .start()
+      .then(() => {
+        recordStatus.innerText = "Recording...";
+        startRecordingBtn.disabled = true;
+        stopRecordingBtn.disabled = false;
+      })
+      .catch((err) => console.error(err));
+  };
+
+  stopRecordingBtn.onclick = () => {
+    let recordStatus = document.getElementById("record-status");
+
+    // Stop recording the conference
+    VoxeetSDK.recording
+      .stop()
+      .then(() => {
+        recordStatus.innerText = "";
+        startRecordingBtn.disabled = false;
+        stopRecordingBtn.disabled = true;
+      })
+      .catch((err) => console.error(err));
+  };
+};
+
+// Add a video stream to the web page
+// Remove the video streem from the web page
+// const removeVideoNode = (participant) => {
+//   let videoNode = document.getElementById('video-' + participant.id);
+
+//   if (videoNode) {
+//     videoNode.srcObject = null; // Prevent memory leak in Chrome
+//     videoNode.parentNode.removeChild(videoNode);
+//   }
+// };
+
+// Add a new participant to the list
+// const addParticipantNode = (participant) => {
+//   // If the participant is the current session user, don't add them to the list
+//   if (participant.id === VoxeetSDK.session.participant.id) return;
+
+//   let participantNode = document.createElement('li');
+//   participantNode.setAttribute('id', 'participant-' + participant.id);
+//   participantNode.innerText = `${participant.info.name}`;
+
+//   const participantsList = document.getElementById('participants-list');
+//   participantsList.appendChild(participantNode);
+// };
+
+// Remove a participant from the list
+
+// Add a screen share stream to the web page
+
+// Remove the screen share stream from the web page

f39eb694a280c81a71b3e783d54ca78260c96ffa
Ashish Kashyap <akk312000@gmail.com>
2021-11-27 21:40:10 +0530

    added icons

--- src/components/Index/AppContainer.js
+++ src/components/Index/AppContainer.js
@@ -1,90 +1,140 @@
-import React, { Component } from 'react'
-import {main} from "../scripts/client";
+import React, { Component } from "react";
+import { main } from "../scripts/client";
 
-import { Icon } from 'semantic-ui-react'
+import { Icon } from "semantic-ui-react";
 export default class AppContainer extends Component {
-    
-    // componentDidMount(){
-    //     main()
-    // };
+  // componentDidMount(){
+  //     main()
+  // };
 
-    state = {
-        name: "",
-        showHome: true,
-        meetingCode: "",
-    }
+  state = {
+    name: "",
+    showHome: true,
+    meetingCode: "",
+  };
 
-    handleSubmit = () => {
-        main(this.state.name);
-        this.setState({showHome: false});
-    }
-    
-    render() {
-        
-        return (this.state.showHome?(
-        <div>
-            <h1>Gozzip</h1>
-            <form onSubmit={this.handleSubmit}>
-                <div>
-                    <p>Enter your name</p>
-                    <input required type="text" value={this.state.name} onChange={(event)=>{this.setState({name: event.target.value})}} />
-                    <p>Create/Enter Meeting Code</p>
-                    <input required type="text" value={this.state.meetingCode} onChange={(event)=>{this.setState({meetingCode: event.target.value})}} />
-                    <br/><br/>
-                </div>
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
-        ):(
-            <div>
-                Gozzip
-                <div id="app">
-        <h1 id="name-message">Logging in...</h1>
+  handleSubmit = () => {
+    main(this.state.name);
+    this.setState({ showHome: false });
+  };
 
-        {/* <!-- Join or Leave a Conference --> */}
-        <div id="form">
+  render() {
+    return this.state.showHome ? (
+      <div>
+        <h1>Gozzip</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <p>Enter your name</p>
+            <input
+              required
+              type="text"
+              value={this.state.name}
+              onChange={(event) => {
+                this.setState({ name: event.target.value });
+              }}
+            />
+            <p>Create/Enter Meeting Code</p>
+            <input
+              required
+              type="text"
+              value={this.state.meetingCode}
+              onChange={(event) => {
+                this.setState({ meetingCode: event.target.value });
+              }}
+            />
+            <br />
+            <br />
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    ) : (
+      <div>
+        <Icon name="conversation"/>Gozzip
+        <div id="app">
+          <h1 id="name-message">Logging in...</h1>
+
+          {/* <!-- Join or Leave a Conference --> */}
+          <div id="form">
             <label>Conference alias :</label>
             <input id="alias-input" value={this.state.meetingCode} />
-            <button id="join-btn" disabled>Join</button>
-            <button id="leave-btn" disabled>Leave</button>
+            <button id="join-btn" disabled>
+             <span class="material-icons">
+video_call
+</span>
+            </button>
+            <button id="leave-btn" disabled>
+             <span class="material-icons">
+call_end
+</span>
+            </button>
             <label id="label-dolby-voice"></label>
-        </div>
-        
-        {/* <!-- Actions on Video and Screen Share --> */}
-        <div id="actions">
-            <button id="start-video-btn" disabled>Start video</button>
-            <button id="stop-video-btn" disabled>Stop video</button>
+          </div>
+
+          {/* <!-- Actions on Video and Screen Share --> */}
+          <div id="actions">
+            <button id="start-video-btn" disabled>
+             <span class="material-icons">
+videocam
+</span>
+            </button>
+            <button id="stop-video-btn" disabled>
+
+<span class="material-icons">
+videocam_off
+</span>
+
+            </button>
             <button id="start-audio-btn" disabled>
-                
-            <Icon  name='microphone' />
-               </button>
+             <span class="material-icons">
+mic
+</span>
+            </button>
 
-          
-            <button id="stop-audio-btn" disabled>  <Icon name='microphone slash' /></button>
-            <button id="start-screenshare-btn" disabled>Start screen share</button>
-            <button id="stop-screenshare-btn" disabled>Stop screen share</button>
-        </div>
+            <button id="stop-audio-btn" disabled>
+              {" "}
+              <span class="material-icons">
+mic_off
+</span>
+            </button>
+            <button id="start-screenshare-btn" disabled>
+              <span class="material-icons">screen_share</span>
+            </button>
+            <button id="stop-screenshare-btn" disabled>
+              <span class="material-icons">stop_screen_share</span>
+            </button>
+          </div>
 
-        {/* <!-- Recording Actions and Status --> */}
-        <div id="recording">
-            <button id="start-recording-btn" disabled>Start recording</button>
-            <button id="stop-recording-btn" disabled>Stop recording</button>
-            <p id="record-status" style={{color: "red"}}></p>
-        </div>
+          {/* <!-- Recording Actions and Status --> */}
+          <div id="recording">
+            <button id="start-recording-btn" disabled>
+              <span class="material-icons">
+radio_button_unchecked
+</span>
+            </button>
+            <button id="stop-recording-btn" disabled>
+            <span class="material-icons">
+radio_button_checked
+</span>
+            </button>
+            <p id="record-status" style={{ color: "red" }}></p>
+          </div>
 
-        {/* <!-- Display the list of participants --> */}
-        <div id="participants">
-            <h3>Participants</h3>
+          {/* <!-- Display the list of participants --> */}
+          <div id="participants">
+            <h3><span class="material-icons">
+people_outline
+</span>Participants</h3>
             <ul id="participants-list"></ul>
+          </div>
+
+          {/* <!-- Container for the video streams --> */}
+          <div id="video-container"></div>
+
+          {/* <!-- Container for the Screen Share --> */}
+          <div id="screenshare-container"></div>
         </div>
-
-        {/* <!-- Container for the video streams --> */}
-        <div id="video-container"></div>
-
-        {/* <!-- Container for the Screen Share --> */}
-        <div id="screenshare-container"></div>
-    </div>
-            </div>
-        ))
-    }
+      </div>
+    );
+  }
 }
